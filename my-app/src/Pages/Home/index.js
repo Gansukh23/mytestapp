a24009139d81e8d484ac8d4b1a74c1028c1cbbca
Gansukh23 <lhagva.gansuh99@gmail.com>
2021-01-25 15:58:33 +0800

    gg

---
+++ my-app/src/Pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+const HomePage = () => {
+  return (
+    <>
+      <ul className='navbar'>
+        <li>
+          <img
+            src='/home/usp/apps/Projects/myapps/my-app/src/logo.png'
+            alt='logo'
+          ></img>
+        </li>
+        <li>
+          <a href='#'>ha</a>
+        </li>
+        <li>
+          <a href='#'>hha</a>
+        </li>
+        <li>
+          <a href='#'>hha</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+export default HomePage;
